fixed attenuation calculation script

src/hamiltonianMC_nf.py
@@ -1,95 +1,36 @@
 import torch
 import numpy as np
 
-class custom_energy(torch.autograd.Function):
-    """
-    Bridges the pure PyTorch/NumPy hybrid log-posterior directly into Pyro.
-    """
-
-    def __init__(self, model_nf, model_pca):
+class custom_potential:
+    def __init__(self, model_nf, model_pca, attenu_obs):
         self.model_nf = model_nf
         self.model_pca = model_pca
- 
-    def forward(self, ctx, x):
-        # x in target distribution space
+        self.attenu_obs = torch.tensor(attenu_obs)
 
-        print('forward')
-        ctx.save_for_backward(x)
-        
-        with torch.no_grad():
-            lp = self.model_nf.log_prob(x)
-            if isinstance(lp, torch.Tensor):
-                lp = lp.item()
-                
-        return torch.tensor(-lp, dtype=x.dtype, device=x.device)
+        #self.gaussian_obs_image = torch.distributions.Normal(torch.tensor(attenu_obs), torch.full(attenu_obs.shape, 0.1))
 
-    def backward(self, ctx, grad_output):
+    def log_posterior(self, params):
+        x = params['x']
 
-        print('backward')
-        x, = ctx.saved_tensors
-        x_np = x.detach().cpu().numpy()
+        log_p_prior = self.model_nf.log_prob(x)
 
-        # log_likelihood = 
+        z = self.model_nf.inverse(x)
+        z_image = torch.matmul(z, torch.tensor(self.model_pca.components_).float()) + torch.tensor(self.model_pca.mean_)
+        #gaussian_z_image = torch.distributions.Normal(z_image, torch.full(attenu_obs.shape, 0.1))
 
-        log_posterior = self.model_nf.log_prob(x) # +
-        
-        # CRITICAL FIX: Re-enable gradient tracking so the internal AD graph can build!
-        with torch.enable_grad():
-            grad_tensor = torch.autograd.grad(self.model_nf.log_prob(x), x, retain_graph= True)
-        
-        grad_potential = grad_tensor.detach().numpy()
-        
-        return grad_tensor * grad_output
+        #print(gaussian_z_image.log_prob(self.attenu_obs)[0][0])
+
+        mean_se = - torch.mean((self.attenu_obs.detach().clone() - z_image) ** 2)
+
+        mean_se = mean_se * 5000
+        #print(mean_se)
+        #print(log_p_prior)
+
+        #print(mean_se)
+        #print(log_p_prior)
+        #print(torch.max(z_image))
+        # print(model_nf.log_prob(x))
+        #print(MSE)
+        #print(model_nf.log_prob(x))
     
-class whitened_potential:
-    """ 
-    Apply a coordinate transformation to Hamiltonian Monte Carlo (HMC) to 
-    make posterior near MAP more isotropic. 
-    """
-    def __init__(self, X_opt_tensor, L, V, gmm, beta,beta_w,
-                       Tpmp, Eb_mean, Eb_std, dw, df, Eb_epsilon):
-        self.X_opt_tensor = X_opt_tensor
-        self.L = L
-        self.V = V
-        self.gmm = gmm
-        self.beta = beta
-        self.beta_w = beta_w
-        self.Tpmp = Tpmp
-        self.Eb_mean = Eb_mean
-        self.Eb_std = Eb_std
-        self.dw = dw
-        self.df = df
-        self.Eb_epsilon = Eb_epsilon
-
-    def __call__(self, params_dict):
-        u = params_dict["u"]
-        x = self.X_opt_tensor + torch.matmul(self.L, u)
-
-        return custom_energy.apply(
-            x, self.V, self.gmm, self.beta, self.beta_w, self.Tpmp,
-            self.Eb_mean, self.Eb_std, self.dw, self.df, self.Eb_epsilon
-        )
-    
-class regular_potential:
-    """
-    Direct potential in x-space (no whitening/Hessian preconditioning).
-    Used for exploratory runs where the posterior landscape is unknown.
-    """
-    def __init__(self, V, gmm, beta, beta_w, Tpmp, Eb_mean, Eb_std, dw, df, Eb_epsilon):
-        self.V = V
-        self.gmm = gmm
-        self.beta = beta
-        self.beta_w = beta_w
-        self.Tpmp = Tpmp
-        self.Eb_mean = Eb_mean
-        self.Eb_std = Eb_std
-        self.dw = dw
-        self.df = df
-        self.Eb_epsilon = Eb_epsilon
-
-    def __call__(self, params_dict):
-        x = params_dict["x"]
-        return custom_energy.apply(
-            x, self.V, self.gmm, self.beta, self.beta_w, self.Tpmp,
-            self.Eb_mean, self.Eb_std, self.dw, self.df, self.Eb_epsilon
-        )
+        return -(log_p_prior + mean_se)

src/ice.py
@@ -144,11 +144,11 @@ def temperature_to_atten_rate_ice(T):
     epsr = 3.17 # real relative permittivity
 
     sigma = sigma0 * np.exp(-(E0/k)*(1/T - 1/Tr))
-    atten_rate = sigma / (c * eps0 * np.sqrt(epsr) / (1000*(10*np.log(np.exp(1)))))
+    atten_rate = sigma * ((1000*(10*np.log10(np.exp(1)))) / (c * eps0 * np.sqrt(epsr)))
 
     return atten_rate
 
-def atten_rate_to_temperature_ice(Na):
+def atten_rate_to_temperature_ice(atten_rate):
     """
     compute temperature from attenuation rate (Np/m)
     assuming pure ice
@@ -161,7 +161,7 @@ def atten_rate_to_temperature_ice(Na):
     eps0 = 8.854e-12 # permittivity in free space
     epsr = 3.17 # real relative permittivity
 
-    sigma = Na * (c * eps0 * np.sqrt(epsr) / (1000*(10*np.log(np.exp(1)))))
+    sigma = atten_rate * ((c * eps0 * np.sqrt(epsr)) / (1000*(10*np.log10(np.exp(1)))))
     T = 1 / ((-k/E0)*np.log(sigma/sigma0) + 1/Tr)
 
     return T